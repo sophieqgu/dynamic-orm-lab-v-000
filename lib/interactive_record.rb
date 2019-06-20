5325205965d62deb4199887a954b11080fa835c8
Sophie Gu <sophieqgu@gmail.com>
2019-06-20 04:34:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,13 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
+  
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE name = '#{name}'
+    SQL
+    DB[:conn].execute(sql)
+  end 
 end
