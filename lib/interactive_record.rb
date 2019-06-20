5a5521516a78804b650d8adaf313cfad3c38ec74
Sophie Gu <sophieqgu@gmail.com>
2019-06-20 04:56:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,9 +38,11 @@ class InteractiveRecord
   
   
   def values_for_insert 
-    self.class.column_names.delete_if {|column_name| column_name == "id"}.map do |column_name|
-      "'#{send(column_name)}'"
-    end.join(", ")
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end 
   
   
