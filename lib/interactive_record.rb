e3360facb19117c17ce81f7dbe59b665d6f33fc7
Sophie Gu <sophieqgu@gmail.com>
2019-06-20 04:00:48 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,7 +32,7 @@ class InteractiveRecord
   end 
   
   
-  def col_name_for_insert 
+  def col_names_for_insert 
     self.class.column_names.delete_if {|column_name| column_name == "id"}.join(", ")
   end 
   
